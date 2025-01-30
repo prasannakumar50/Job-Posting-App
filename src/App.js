d212385413e08e1281322cfc71a774a8fce22204
prasannakumar50 <babblu293@gmail.com>
2025-01-30 15:45:37 +0530

    feat: create react app using npx

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='container py-2'>
+      <h1 >Welcome to the playground</h1>
+      
+    </div>
+  );
+}
+
+export default App;
